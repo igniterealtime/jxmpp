8bf645bc28de3a8141e43f510259eebc095815c1
Florian Schmaus <flo@geekplace.eu>
2014-06-02 18:23:34 +0200

    Extend completeJidFrom

--- jxmpp-core/src/main/java/org/jxmpp/util/XmppStringUtils.java
+++ jxmpp-core/src/main/java/org/jxmpp/util/XmppStringUtils.java
@@ -324,10 +324,17 @@ public class XmppStringUtils {
 		return res;
 	}
 
+	public static String completeJidFrom(String localpart, String domainpart) {
+		return completeJidFrom(localpart, domainpart, null);
+	}
+
 	public static String completeJidFrom(String localpart, String domainpart, String resource) {
-		int localpartLength = localpart.length();
+		if (domainpart == null) {
+			throw new IllegalArgumentException("domainpart must not be null");
+		}
+		int localpartLength = localpart != null ? localpart.length() : 0;
 		int domainpartLength = domainpart.length();
-		int resourceLength = resource.length();
+		int resourceLength = resource != null ? resource.length() : 0;
 		int maxResLength = localpartLength + domainpartLength + resourceLength + 2;
 		StringBuilder sb = new StringBuilder(maxResLength);
 		if (localpartLength > 0) {
