91fb9f008e7301c221d5888b855b59b7b357cca3
Florian Schmaus <flo@geekplace.eu>
2015-01-24 10:56:11 +0100

    Add empty String shortcut to XmppStringPrepUtil

--- jxmpp-core/src/main/java/org/jxmpp/stringprep/XmppStringPrepUtil.java
+++ jxmpp-core/src/main/java/org/jxmpp/stringprep/XmppStringPrepUtil.java
@@ -35,6 +35,8 @@ public class XmppStringPrepUtil {
 		if (xmppStringprep == null) {
 			return string;
 		}
+		// Avoid cache lookup if string is the empty string
+		throwIfEmptyString(string);
 		String res = NODEPREP_CACHE.get(string);
 		if (res != null) {
 			return res;
@@ -48,6 +50,8 @@ public class XmppStringPrepUtil {
 		if (xmppStringprep == null) {
 			return string;
 		}
+		// Avoid cache lookup if string is the empty string
+		throwIfEmptyString(string);
 		String res = DOMAINPREP_CACHE.get(string);
 		if (res != null) {
 			return res;
@@ -61,6 +65,8 @@ public class XmppStringPrepUtil {
 		if (xmppStringprep == null) {
 			return string;
 		}
+		// Avoid cache lookup if string is the empty string
+		throwIfEmptyString(string);
 		String res = RESOURCEPREP_CACHE.get(string);
 		if (res != null) {
 			return res;
@@ -74,5 +80,18 @@ public class XmppStringPrepUtil {
 		NODEPREP_CACHE.setMaxCacheSize(size);
 		DOMAINPREP_CACHE.setMaxCacheSize(size);
 		RESOURCEPREP_CACHE.setMaxCacheSize(size);
+	}
+
+	/**
+	 * Throws a XMPP Stringprep exception if string is the empty string.
+	 *
+	 * @param string
+	 * @throws XmppStringprepException
+	 */
+	private static void throwIfEmptyString(String string) throws XmppStringprepException {
+		// TODO replace with string.isEmpty() once Smack's min Android SDK level is > 8
+		if (string.length() == 0) {
+			throw new XmppStringprepException(string, "Argument can't be the empty string");
+		}
 	}
 }
